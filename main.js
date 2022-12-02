fba6a57b6b1d15cb9aaed0e132b8ef801855de46
unknown <araujo.lericke@gmail.com>
2022-12-02 18:45:36 -0300

    adicionando while

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-
-function tocaSomPom (){
+// POM
+function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
 
-
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let  contador = 0;
+
+while (contador < 9) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+
+}
