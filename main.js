d20215bb6846357e7e900f070cd28960e194b2b8
lericke <araujo.lericke@gmail.com>
2022-12-06 21:14:22 -0300

    Implementando propriedades de teclado

--- main.js
+++ main.js
@@ -10,11 +10,21 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`; // Template string
-    
-    console.log(idAudio);
 
     tecla.onclick = function () {
-        tocaSom(idAudio   )
+        tocaSom(idAudio);
+    }
+    
+    tecla.onkeydown = function (event) {
+
+        console.log(event.code);
+
+        if (event.code === 'Space' || event.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
     }
 
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
